f1dadf332ae44c48a950cabb95f565ccbf241235
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-06-21 16:07:39 -0300

    FEAT: remove pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        // Configura los detalles de tu manifest.json
-        name: "Mi PWA React",
-        short_name: "MiPWA",
-        start_url: "/index.html",
-        display: "standalone",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
