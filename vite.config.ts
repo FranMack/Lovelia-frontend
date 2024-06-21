4311c60f37c7e1c73af00c9799132135d55f7567
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-06-21 13:22:35 -0300

    FEAT: remove app folder & add Procfile and edit vite config

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        // Configura los detalles de tu manifest.json
+        name: "Mi PWA React",
+        short_name: "MiPWA",
+        start_url: "/index.html",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+  },
+});
