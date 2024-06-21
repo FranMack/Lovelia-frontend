4de7eecc95560eefa7171be275103da5b315f923
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-06-21 15:17:40 -0300

    FEAT: add declaration.d.ts

---
+++ declarations.d.ts
@@ -0,0 +1,11 @@
+declare module "react-dom/client";
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
