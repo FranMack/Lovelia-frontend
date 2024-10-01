cde7f81e5024f8de3f106fcb55f52679cf220459
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-10-01 16:58:16 -0300

    FEAT: remove unused variables

--- src/store/pages/Store3.tsx
+++ src/store/pages/Store3.tsx
@@ -1,46 +1,35 @@
+import {
+  Position,
+  Template1,
+  Template1Options,
+} from "../../ui/pages/Template1";
 import gift from "../assets/tienda_regalo.png";
-import { ButtonArrowRight2 } from "../../ui/components/ButtonArrowRight2";
-import { Template1 } from "../../ui/pages/Template1";
-import { Template1Options } from "../../ui/pages/Template1";
-import { Position } from "../../ui/pages/Template1";
 
-const buttonRightInfo={
-    text:"VER OPCIONES",
-    color:"#6f3289",
-    onclick:()=>{}
-}
-
-const templateOptions:Template1Options={
-  image:gift,
-  position:Position.Left,
- color:"#6f3289",
-  backgroundColor:"#EDC7B9"
-}
+const templateOptions: Template1Options = {
+  image: gift,
+  position: Position.Left,
+  color: "#6f3289",
+  backgroundColor: "#EDC7B9",
+};
 
 export const Store3 = () => {
-
   return (
-   <Template1 {...templateOptions} >
-    <div className="section3-store-internal-container right">
-    <div className="section3-store-internal-text-container">
+    <Template1 {...templateOptions}>
+      <div className="section3-store-internal-container right">
+        <div className="section3-store-internal-text-container">
+          <div className="title-container">
+            <h2>REGALA UN TALISMÁN</h2>
+          </div>
 
-        <div className="title-container">
-        <h2>
-  REGALA UN TALISMÁN
-  </h2>
-  
+          <p>
+            Cuando regalas un talismán, brindas a<br /> alguien querido{" "}
+            <strong>
+              la oportunidad de
+              <br /> conectar con su deseo o propósito.
+            </strong>
+          </p>
         </div>
-  
-  
-
-  <p>
-  Cuando regalas un talismán, brindas a<br/> alguien querido <strong>la oportunidad de<br/> conectar con su deseo o propósito.</strong>
-  </p>
-  
-  
-
-  </div>
-</div>
-</Template1>
+      </div>
+    </Template1>
   );
 };
