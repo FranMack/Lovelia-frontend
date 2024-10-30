b8bf5497183e4d6778dca459b9b451a40b8cd875
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-10-30 17:28:41 -0300

    FEAT: update service worker firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 // firebase-messaging-sw.js
 
-// Import Firebase scripts (important for web push notifications)
+// Import Firebase scripts
 importScripts(
   "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
 );
@@ -9,14 +9,14 @@ importScripts(
   "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 );
 
-// Firebase configuration (same as in your app)
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAb__3b4qmoMjUyiL01SvT6FAM25Ggbojo",
-  authDomain: "lovelia-9859c.firebaseapp.com",
-  projectId: "lovelia-9859c",
-  storageBucket: "lovelia-9859c.appspot.com",
-  messagingSenderId: "1038148980896",
-  appId: "1:1038148980896:web:d906cec37e5cca2dc439c5",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 // Initialize Firebase
@@ -29,16 +29,36 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message: ", payload);
 
-  // Customize notification
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: "/firebase-logo.png",
+    data: {
+      url: payload.data.click_action || "https://www.lovelia.me"
+    },
   };
 
-  // Show notification
   return self.registration.showNotification(
     notificationTitle,
     notificationOptions
+  );
+});
+
+// Notification click event listener
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close(); // Close the notification on click
+
+  // Perform an action with the URL specified in notification data
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url === event.notification.data.url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(event.notification.data.url);
+      }
+    })
   );
 });
