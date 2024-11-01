1b42c431320eca0b2c73c1fa23aa6d24a994feb4
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-11-01 10:24:48 -0300

    FEAT: update service worker with plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from 'vite-plugin-pwa';
 
+
 export default defineConfig({
-  plugins: [react(),  VitePWA()],
+  plugins: [react(),  VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    }
+  })],
   build: {
     outDir: "dist",
   },
