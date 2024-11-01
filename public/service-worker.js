8c794c3c3dd51b253eb385bb4adf771be6be6564
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-11-01 09:50:58 -0300

    FEAT: update service-worker without empty cache and reload page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,19 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/vite.svg',
-          '/isologo-lovelia.png',
-          // Add any other assets or routes you need to cache
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  self.skipWaiting(); // Forces the waiting service worker to become active
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/vite.svg',
+        '/isologo-lovelia.png',
+        // Add other assets or routes to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  clients.claim(); // Takes control of any clients as soon as the service worker activates
+  console.log('Service worker activated');
+});
