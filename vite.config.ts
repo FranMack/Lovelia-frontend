8577a446e7cf343ff8b7904f9d752dfbf28dc728
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-11-01 10:00:45 -0300

    FEAT: update service-worker without empty cache and reload page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  VitePWA()],
   build: {
     outDir: "dist",
   },
