0f5eb76d3a89c0b439a411061bf9a44e2da6882a
FranMack <franmack556@gmail.com>
2025-01-07 13:53:23 -0300

    FEAT: video background bugfix atemp

--- src/ui/components/BackgroundVideo.tsx
+++ src/ui/components/BackgroundVideo.tsx
@@ -27,7 +27,7 @@ export function BackgroundVideo() {
     <>
       <section className="video-container">
         <div className="auxiliar-video-container">
-          <video autoPlay muted loop ref={videoRef}>
+          <video autoPlay muted loop playsInline ref={videoRef}>
             <source src={videoHome} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
