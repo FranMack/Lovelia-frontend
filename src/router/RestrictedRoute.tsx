9a5d99c7a85480ecbfbb5785a1fd2c7c35df68fd
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2025-01-07 11:16:19 -0300

    FEAT: remove mercadopagocheckoutform

--- src/router/RestrictedRoute.tsx
+++ src/router/RestrictedRoute.tsx
@@ -1,23 +1,21 @@
-import { ReactNode, useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { UserContext } from '../context';
+import {ReactNode} from 'react';
+import {Navigate, useLocation} from 'react-router-dom';
 
 interface Props {
   children: ReactNode;
 }
 
-export function RestrictedRoute({ children }: Props) {
-  const { email } = useContext(UserContext);
+export function RestrictedRoute({children}: Props) {
   const location = useLocation(); // Importante: se necesita useLocation para obtener location.pathname
 
-  const userSessionCookie = document.cookie.includes("token");
+  const userSessionCookie = document.cookie.includes('token');
 
   let pathToRedirect = '/';
 
   // Lógica de redirección
   if (
     userSessionCookie &&
-    (location.pathname.includes('/checkout/digital') || 
+    (location.pathname.includes('/checkout/digital') ||
       localStorage.getItem('pathToRedirect'))
   ) {
     pathToRedirect = '/checkout/digital';
