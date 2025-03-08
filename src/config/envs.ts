593d6bef64b8a803e811a466b6349838235444eb
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2025-03-08 14:00:05 -0300

    FEAT: move all key/values to .env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,12 +1,11 @@
 export const envs = {
-  //API_DOMAIN: 'http://localhost:3000',
-  API_DOMAIN: 'https://api.lovelia.me',
+  API_DOMAIN: import.meta.env.API_DOMAIN,
 
   // Firebase credentials
-  FIREBASE_API_KEY: 'AIzaSyAb__3b4qmoMjUyiL01SvT6FAM25Ggbojo',
-  FIREBASE_AUTH_DOMAIN: 'lovelia-9859c.firebaseapp.com',
-  FIREBASE_PROJECT_ID: 'lovelia-9859c',
-  FIREBASE_STORAGE_BUCKET: 'lovelia-9859c.appspot.com',
-  FIREBASE_MESSAGING_SENDER_ID: '1038148980896',
-  FIREBASE_APP_ID: '1:1038148980896:web:d906cec37e5cca2dc439c5',
+  FIREBASE_API_KEY: import.meta.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: import.meta.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: import.meta.env.FIREBASE_APP_ID,
 };
